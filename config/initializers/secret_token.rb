eb3097d6306ea2bf6277f7546eedc659a2b6608f
Nastya Davidova <chudik@hotmail.com>
2011-05-17 14:40:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sashka::Application.config.secret_token = 'bbfb621012098bc91c7f228914ac93baaab5d466c4050dac2b6252053e2c6823364ad128d5c495a0611cba7941b47688e949473e518ff0cbf0fa5bb3cab2d718'
